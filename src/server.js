e7f5e71d6c06b5c944a24141f9ff87953a7642af
Mark Guo <markgk629@gmail.com>
2018-12-19 12:47:44 +1100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => {
+  console.log('listening at port:', PORT)
+})
