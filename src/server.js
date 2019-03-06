a8951e907453c37089f525a0923e75936841a047
Mark Guo <markgk629@gmail.com>
2019-03-06 12:47:46 +1100

    [server] Update upload image file & response handling

--- src/server.js
+++ src/server.js
@@ -14,6 +14,9 @@ const fileType = require('file-type')
 
 const { Asset } = require('./models')
 
+const aws = require('aws-sdk')
+const fs = require('fs')
+
 // Initialize knex.
 const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 // Bind all Models to a knex instance. If you only have one database in
@@ -23,10 +26,8 @@ Model.knex(knex)
 
 app.use(express.static('public'))
 
-const multer = require('multer')
-const multerS3 = require('multer-s3')
-const aws = require('aws-sdk')
-const fs = require('fs')
+// const multer = require('multer')
+// const multerS3 = require('multer-s3')
 
 app.use('/graphql', graphqlHandler)
 app.use(morgan('dev'))
@@ -41,19 +42,19 @@ const s3 = new aws.S3()
 
 const bucketName = 'png.switchmaven.com'
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: bucketName,
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname })
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    }
-  })
-})
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: bucketName,
+//     acl: 'public-read',
+//     metadata: function (req, file, cb) {
+//       cb(null, { fieldName: file.fieldname })
+//     },
+//     key: function (req, file, cb) {
+//       cb(null, Date.now().toString())
+//     }
+//   })
+// })
 
 app.get('/accounts', async (req, res) => {
   var { Account } = require('./models')
@@ -64,9 +65,20 @@ app.get('/accounts', async (req, res) => {
 
 // const singleUpload = upload.single('image')
 
+const getFilePath = function ({ asset_id, event_id }) {
+  const eventID = event_id || 1
+
+  return `${asset_id}/${eventID}/`
+}
+
 const saveTemp = function (req, res, next) {
-  var fileName = req.headers['file-name']
-  var wstream = fs.createWriteStream('./tmp/' + fileName)
+  const fileName = req.headers['file-name']
+  const filePath = getFilePath(req.query || {})
+  const tmpPath = `./tmp/${filePath}`
+
+  fs.mkdirSync(tmpPath, { recursive: true })
+
+  const wstream = fs.createWriteStream(tmpPath + fileName)
 
   return new Promise((resolve, reject) => {
     req.on('data', function (chunk) {
@@ -76,10 +88,7 @@ const saveTemp = function (req, res, next) {
     req.on('end', function (chunk) {
       wstream.on('finish', function () {
         console.log('wstream path', wstream.path)
-        resolve(wstream.path)
-
-        res.writeHead(200, 'OK', { 'Content-Type': 'text/html' })
-        res.end()
+        resolve({ filePath, tmpName: fileName })
       })
 
       console.log('file has been written')
@@ -97,44 +106,57 @@ const saveTemp = function (req, res, next) {
 app.post('/image-upload', function (req, res) {
   console.log('query', req.query)
 
-  return saveTemp(req, res).then(filePath => {
-    const file = fs.readFileSync(filePath)
-    const type = fileType(file)
+  return saveTemp(req, res)
+    .then(({ filePath, tmpName }) => {
+      const tmpPath = `./tmp/${filePath}`
+      const file = fs.readFileSync(tmpPath + tmpName)
+      const type = fileType(file)
 
-    const base64 = new Buffer(file).toString('base64')
-    const base64Data = new Buffer(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+      const base64 = new Buffer(file).toString('base64')
+      const base64Data = new Buffer(
+        base64.replace(/^data:image\/\w+;base64,/, ''),
+        'base64'
+      )
 
-    const folder = `${req.query.asset_id}/`
-    const fileName = `pig-${Date.now()}.${type.ext}`
-    const params = {
-      Bucket: bucketName,
-      Key: folder + fileName,
-      ACL: 'public-read',
-      Body: base64Data,
-      ContentType: type.mime
-    }
-
-    console.log('size', fs.statSync(filePath).size)
-    console.log('Folder name: ' + folder)
-    console.log('File: ' + fileName)
-
-    s3.putObject(params, function (err, data) {
-      if (err) {
-        console.log('Error: ', err)
-      } else {
-        console.log('s3', data)
+      const fileName = `pig-${Date.now()}.${type.ext}`
+      const params = {
+        Bucket: bucketName,
+        Key: filePath + fileName,
+        ACL: 'public-read',
+        Body: base64Data,
+        ContentType: type.mime
       }
-    })
-    const s3URL = `https://s3-ap-southeast-1.amazonaws.com/${bucketName}/${folder}${fileName}`
-    console.log('found at', s3URL)
 
-    Asset.query()
-      .update({ image: s3URL })
-      .where('id', req.query.asset_id)
-      .then((res) => {
-        console.log('updated asset image path', res)
+      console.log('size', fs.statSync(tmpPath).size)
+      console.log('Folder name: ' + filePath)
+      console.log('File: ' + fileName)
+
+      s3.putObject(params, function (err, data) {
+        if (err) {
+          console.log('Error: ', err)
+        } else {
+          console.log('s3', data)
+        }
       })
-  }).catch(console.error)
+      const s3URL = `https://s3-ap-southeast-1.amazonaws.com/${bucketName}/${filePath}${fileName}`
+      console.log('found at', s3URL)
+
+      Asset.query()
+        .update({ image: s3URL })
+        .where('id', req.query.asset_id)
+        .then(asset => {
+          console.log('updated asset image path', asset)
+
+          res.writeHead(200, 'OK', { 'Content-Type': 'text/html' })
+          res.end()
+
+          fs.unlink(tmpPath + tmpName, (err) => {
+            if (err) { console.log(err) }
+            console.log('deleted file:', tmpPath + tmpName)
+          })
+        })
+    })
+    .catch(console.error)
 })
 
 app.get('/', (req, res) => {
